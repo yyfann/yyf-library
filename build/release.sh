42dd76c54a59506f337b908be1b214954fdb4c33
yyfann <a1097833559@163.com>
2020-05-07 17:07:27 +0800

    [build] 1.1.5

--- build/release.sh
+++ build/release.sh
@@ -8,3 +8,12 @@
 VERSION=`npx select-version-cli`
 
 echo "Releasing $VERSION ..."
+
+# 更新版本号, 自动打标签 (之前要先提交一波才行)
+git add -A
+git commit -m "[build] $VERSION"
+npm version $VERSION --message "[release] $VERSION"
+
+git push
+
+npm publish
